05a1259c4854a20aa847655dd50b889830328ac7
pietroborrello <pietro.borrello95@gmail.com>
2018-09-06 16:19:24 +0200

    fix

--- qkiller.c
+++ qkiller.c
@@ -8,7 +8,7 @@
 #include <sys/ioctl.h>
 #include <sys/types.h> /* for pid_t */
 #include <sys/wait.h>  /* for wait */
-#include <linux/ptrace.h>
+#include <sys/ptrace.h>
 #include <sys/user.h>
 #include <linux/perf_event.h>
 #include <asm/unistd.h>
@@ -39,6 +39,27 @@ struct read_format
         uint64_t value;
         uint64_t id;
     } values[];
+};
+
+struct i386_user_regs_struct
+{
+    uint32_t ebx;
+    uint32_t ecx;
+    uint32_t edx;
+    uint32_t esi;
+    uint32_t edi;
+    uint32_t ebp;
+    uint32_t eax;
+    uint32_t xds;
+    uint32_t xes;
+    uint32_t xfs;
+    uint32_t xgs;
+    uint32_t orig_eax;
+    uint32_t eip;
+    uint32_t xcs;
+    uint32_t eflags;
+    uint32_t esp;
+    uint32_t xss;
 };
 
 static long
